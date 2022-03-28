5ac0018e1b618bb9956e4ccdac73f1c26e23d8d0
Y Zhang <91054065+MrX1357@users.noreply.github.com>
2022-03-28 22:13:22 +0000

    I allow the invalid input for junior division to be sort out in the age part.

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import time
 import random
 
 # Welcome
-print("Welcome to the x-quiz, which you will be test out your mathematical ablity. Are you excited?")
+print(" Welcome to the x-quiz, which you will be test out your mathematical ablity. Are you excited? ")
 print("---------------------------------------------------------------------------")
 time.sleep(2)
-print("You will only be notified in your answer if you entered an invalid case. All other time, it will automatically jump to the next question")
+print(" You will only be notified in your answer if you entered an invalid case. All other time, it will automatically jump to the next question ")
 time.sleep(2)
-print("There are 15 questions for you to ask, some are hard, and some are easy. Hard questions are worth 2 points whereas easy question only worth 1 point. The question will be given to you randomly.")
+print(" There are 15 questions for you to ask, some are hard, and some are easy. Hard questions are worth 2 points whereas easy question only worth 1 point. The question will be given to you randomly. ")
 time.sleep(2)
 
 # Call and Name
@@ -30,7 +30,7 @@ point = 0
 # Functions
 # If the user got selected a choice that is not in letters during junior division or the first question in senior division, they will be notfied that and they will need to reenter the value.
 def explain():
-  print("This is not a valid solution, please only select number")
+  print("This is not a valid solution, please only select letter")
 # If the user got selected a choice that is not the required alphabet, they will be notified by this and they will need to reenter the value.
 def letter():
   print("Please only select between the choice of a,b,c,d")
@@ -158,252 +158,225 @@ question_list_seniorplus = ["Calculate the deriative of 2(x-5)^2",
                            "Solve the x in exponetial equation of 3^(x-5) = 81",
                            "Calculate the value of log 20 + log 5, leave this as a number."]
 
-# Junior quiz function
-def junior_quizbank():
-  junior_1 = input(question_list_junior[0])
-  while not junior_1.isalpha():
-    explain()
-    junior_1 = input(question_list_junior[0]).strip().lower()
-  while junior_1 not in ["a","b","c","d"]:
-    letter()
-    junior_1 = input(question_list_junior[0]).strip().lower()
-  if junior_1 == "c":
-    correct_point_easy()
-  else:
-    correct_point_hard()
+junior_1 = input(question_list_junior[0])
+if junior_1 == "c":
+  correct_point_easy()
+else:
+  correct_point_hard()
     
-  junior_2 = input(question_list_junior[1])
-  while not junior_2.isalpha():
-    explain()
-    junior_2 = input(question_list_junior[1]).strip().lower()
-  while junior_2 not in ["a","b","c","d"]:
-    letter()
-    junior_2 = input(question_list_junior[1]).strip().lower()
-  if junior_2 == "a":
-    correct_point_easy()
-  else:
-    wrong_point()
+junior_2 = question_list_junior[1]
+while junior_2 not in ["a","b","c","d"]:
+  letter()
+  junior_2 = input(question_list_junior[1]).strip().lower()
+if junior_2 == "a":
+  correct_point_easy()
+else:
+  wrong_point()
     
+junior_3 = question_list_junior[2]
+while junior_3 not in ["a","b","c","d"]:
+  letter()
   junior_3 = input(question_list_junior[2]).strip().lower()
-  while not junior_3.isalpha():
-    explain()
-    junior_3 = input(question_list_junior[2]).strip().lower()
-  while junior_3 not in ["a","b","c","d"]:
-    letter()
-    junior_3 = input(question_list_junior[2]).strip().lower()
-  if junior_3 == "a":
-    correct_point_easy()
-  else:
-    wrong_point()
-  
+if junior_3 == "a":
+  correct_point_easy()
+else:
+  wrong_point()
 
+junior_4 = question_list_junior[3]
+while junior_4 not in ["a","b","c","d"]:
+  letter()
   junior_4 = input(question_list_junior[3]).strip().lower()
-  while not junior_4.isalpha():
-    explain()
-    junior_4 = input(question_list_junior[3]).strip().lower()
-  while junior_4 not in ["a","b","c","d"]:
-    letter()
-    junior_4 = input(question_list_junior[3]).strip().lower()
-  if junior_4 == "d":
-    correct_point_easy()
-  else:
-    wrong_point()
+if junior_4 == "d":
+  correct_point_easy()
+else:
+  wrong_point()
  
+junior_5 = question_list_junior[4]
+while junior_5 not in ["a","b","c","d"]:
+  letter()
   junior_5 = input(question_list_junior[4]).strip().lower()
-  while not junior_5.isalpha():
-    explain()
-    junior_5 = input(question_list_junior[4]).strip().lower()
-  while junior_5 not in ["a","b","c","d"]:
-    letter()
-    junior_5 = input(question_list_junior[4]).strip().lower()
-  if junior_5 == "b":
-    correct_point_easy()
-  else:
-    wrong_point()
+if junior_5 == "b":
+  correct_point_easy()
+else:
+  wrong_point()
 
-  junior_6 = input(question_list_junior[5])
-  while not junior_6.isalpha():
-    explain()
-    junior_6 = input(question_list_junior[5])
-  while junior_6 not in ["a","b","c","d"]:
-    letter()
-    junior_6 = input(question_list_junior[5]).strip().letter()
-  if junior_6 == "b":
-    correct_point_easy()
-  else:
-    wrong_point()
+junior_6 = question_list_junior[5]
+while junior_6 not in ["a","b","c","d"]:
+  letter()
+  junior_6 = input(question_list_junior[5]).strip().letter()
+if junior_6 == "b":
+  correct_point_easy()
+else:
+  wrong_point()
 
+junior_7 = question_list_junior[6]
+while junior_7 not in ["a","b","c","d"]:
+  letter()
   junior_7 = input(question_list_junior[6]).strip().lower()
-  while not junior_7.isalpha():
-    explain()
-    junior_7 = input(question_list_junior[6]).strip().lower()
-  while junior_7 not in ["a","b","c","d"]:
-    letter()
-    junior_7 = input(question_list_junior[6]).strip().lower()
-  if junior_7 == "a":
-    correct_point_easy()
-  else:
-    wrong_point()
+if junior_7 == "a":
+  correct_point_easy()
+else:
+  wrong_point()
 
+junior_8 = question_list_junior[7]
+while not junior_8.isalpha():
+  explain()
   junior_8 = input(question_list_junior[7]).strip().lower()
-  while not junior_8.isalpha():
-    explain()
-    junior_8 = input(question_list_junior[7]).strip().lower()
-  while junior_8 not in ["a","b","c","d"]:
-    letter()
-    junior_8 = input(question_list_junior[7]).strip().lower()
-  # There are two correct answers in this choice
-  if junior_8 in ["a","c"]:
-    correct_point_easy()
-  else:
-    wrong_point()
+while junior_8 not in ["a","b","c","d"]:
+  letter()
+  junior_8 = input(question_list_junior[7]).strip().lower()
+# There are two correct answers in this choice
+if junior_8 in ["a","c"]:
+  correct_point_easy()
+else:
+  wrong_point()
 
+junior_9 = question_list_junior[8]
+while not junior_9.isalpha():
+  explain()
   junior_9 = input(question_list_junior[8]).strip().lower()
-  while not junior_9.isalpha():
-    explain()
-    junior_9 = input(question_list_junior[8]).strip().lower()
-  while junior_9 not in ["a","b","c","d"]:
-    letter()
-    junior_9 = input(question_list_junior[8]).strip().lower()
-  if junior_9 == "a":
-    correct_point_easy()
-  else:
-    wrong_point()
+while junior_9 not in ["a","b","c","d"]:
+  letter()
+  junior_9 = input(question_list_junior[8]).strip().lower()
+if junior_9 == "a":
+  correct_point_easy()
+else:
+  wrong_point()
 
+junior_10 = question_list_junior[9]
+while not junior_10.isalpha():
+  explain()
+  junior_10 = question_list_junior[9]
+while junior_10 not in ["a","b","c","d"]:
+  letter()
   junior_10 = input(question_list_junior[9]).strip().lower()
-  while not junior_10.isalpha():
-    explain()
-    junior_10 = input(question_list_junior[9]).strip().lower()
-  while junior_10 not in ["a","b","c","d"]:
-    letter()
-    junior_10 = input(question_list_junior[9]).strip().lower()
-  if junior_10 == "b":
-    correct_point_easy()
-  else:
-    wrong_point()
+if junior_10 == "b":
+  correct_point_easy()
+else:
+  wrong_point()
 # Now the question is getting harder and is now worth two points
+junior_11 = question_list_juniorplus[0]
+while not junior_11.isalpha():
+  explain()
   junior_11 = question_list_juniorplus[0]
-  while not junior_11.isalpha():
-    explain()
-    junior_11 = question_list_juniorplus[0]
-  while junior_11 not in ["a","b","c","d"]:
-    letter()
-    junior_11 = input(question_list_juniorplus[0]).strip().lower()
-  if junior_11 == "d":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_11 not in ["a","b","c","d"]:
+  letter()
+  junior_11 = input(question_list_juniorplus[0]).strip().lower()
+if junior_11 == "d":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_12 = question_list_juniorplus[1]
+while not junior_12.isalpha():
+  explain()
   junior_12 = input(question_list_juniorplus[1]).strip().lower()
-  while not junior_12.isalpha():
-    explain()
-    junior_12 = input(question_list_juniorplus[1]).strip().lower()
-  while junior_12 not in ["a","b","c","d"]:
-    letter()
-    junior_12 = input(question_list_juniorplus[1]).strip().lower()
-  if junior_12 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_12 not in ["a","b","c","d"]:
+  letter()
+  junior_12 = input(question_list_juniorplus[1]).strip().lower()
+if junior_12 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_13 = question_list_juniorplus[2]
+while not junior_13.isalpha():
+  explain()
   junior_13 = input(question_list_juniorplus[2]).strip().lower()
-  while not junior_13.isalpha():
-    explain()
-    junior_13 = input(question_list_juniorplus[2]).strip().lower()
-  while junior_13 not in ["a","b","c","d"]:
-    letter()
-    junior_13 = input(question_list_juniorplus[2]).strip().lower()
-  if junior_13 == "b":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_13 not in ["a","b","c","d"]:
+  letter()
+  junior_13 = input(question_list_juniorplus[2]).strip().lower()
+if junior_13 == "b":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_14 = question_list_juniorplus[3]
+while not junior_14.isalpha():
+  explain()
   junior_14 = input(question_list_juniorplus[3]).strip().lower()
-  while not junior_14.isalpha():
-    explain()
-    junior_14 = input(question_list_juniorplus[3]).strip().lower()
-  while junior_14 not in ["a","b","c","d"]:
-    letter()
-    junior_14 = input(question_list_juniorplus[3]).strip().lower()
-  if junior_14 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_14 not in ["a","b","c","d"]:
+  letter()
+  junior_14 = input(question_list_juniorplus[3]).strip().lower()
+if junior_14 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_15 = question_list_juniorplus[4]
+while not junior_15.isalpha():
+  explain()
   junior_15 = input(question_list_juniorplus[4]).strip().lower()
-  while not junior_15.isalpha():
-    explain()
-    junior_15 = input(question_list_juniorplus[4]).strip().lower()
-  while junior_15 not in ["a","b","c","d"]:
-    letter()
-    junior_15 = input(question_list_juniorplus[4]).strip().lower()
-  if junior_15 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_15 not in ["a","b","c","d"]:
+  letter()
+  junior_15 = input(question_list_juniorplus[4]).strip().lower()
+if junior_15 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_16 = question_list_juniorplus[5]
+while not junior_16.isalpha():
+  explain()
   junior_16 = input(question_list_juniorplus[5]).strip().lower()
-  while not junior_16.isalpha():
-    explain()
-    junior_16 = input(question_list_juniorplus[5]).strip().lower()
-  while junior_16 not in ["a","b","c","d"]:
-    letter()
-    junior_16 = input(question_list_juniorplus[5]).strip().lower()
-  if junior_16 == "d":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_16 not in ["a","b","c","d"]:
+  letter()
+  junior_16 = input(question_list_juniorplus[5]).strip().lower()
+if junior_16 == "d":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_17 = question_list_juniorplus[6]
+while not junior_17.isalpha():
+  explain()
   junior_17 = input(question_list_juniorplus[6]).strip().lower()
-  while not junior_17.isalpha():
-    explain()
-    junior_17 = input(question_list_juniorplus[6]).strip().lower()
-  while junior_17 not in ["a","b","c","d"]:
-    letter()
-    junior_17 = input(question_list_juniorplus[6]).strip().lower()
-  if junior_17 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_17 not in ["a","b","c","d"]:
+  letter()
+  junior_17 = input(question_list_juniorplus[6]).strip().lower()
+if junior_17 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_18 = input(question_list_juniorplus[7]).strip().lower()
+while not junior_18.isalpha():
+  explain()
   junior_18 = input(question_list_juniorplus[7]).strip().lower()
-  while not junior_18.isalpha():
-    explain()
-    junior_18 = input(question_list_juniorplus[7]).strip().lower()
-  while junior_18 not in ["a","b","c","d"]:
-    letter()
-    junior_18 = input(question_list_juniorplus[7]).strip().lower()
-  if junior_18 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_18 not in ["a","b","c","d"]:
+  letter()
+  junior_18 = input(question_list_juniorplus[7]).strip().lower()
+if junior_18 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_19 = input(question_list_juniorplus[8]).strip().lower()
+while not junior_19.isalpha():
+  explain()
   junior_19 = input(question_list_juniorplus[8]).strip().lower()
-  while not junior_19.isalpha():
-    explain()
-    junior_19 = input(question_list_juniorplus[8]).strip().lower()
-  while junior_19 not in ["a","b","c","d"]:
-    letter()
-    junior_19 = input(question_list_juniorplus[8]).strip().lower()
-  if junior_19 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_19 not in ["a","b","c","d"]:
+  letter()
+  junior_19 = input(question_list_juniorplus[8]).strip().lower()
+if junior_19 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
+junior_20 = input(question_list_juniorplus[9]).strip().lower()
+while not junior_20.isalpha():
+  explain()
   junior_20 = input(question_list_juniorplus[9]).strip().lower()
-  while not junior_20.isalpha():
-    explain()
-    junior_20 = input(question_list_juniorplus[9]).strip().lower()
-  while junior_20 not in ["a","b","c","d"]:
-    letter()
-    junior_20 = input(question_list_juniorplus[9]).strip().lower()
-  if junior_20 == "a":
-    correct_point_hard()
-  else:
-    wrong_point()
+while junior_20 not in ["a","b","c","d"]:
+  letter()
+  junior_20 = input(question_list_juniorplus[9]).strip().lower()
+if junior_20 == "a":
+  correct_point_hard()
+else:
+  wrong_point()
 
 #Quizbank in junior division that is going to be given to the user.
-  junior_quizbank =       [junior_1,junior_2,junior_3,junior_4,junior_5,junior_6,junior_7,junior_8,junior_9,junior_10,junior_11,junior_12,junior_13,junior_14,junior_15,junior_16,junior_17,junior_18,junior_19,junior_20]
+junior_quizbank =       [junior_1,junior_2,junior_3,junior_4,junior_5,junior_6,junior_7,junior_8,junior_9,junior_10,junior_11,junior_12,junior_13,junior_14,junior_15,junior_16,junior_17,junior_18,junior_19,junior_20]
 
 # Question in senior division
 def senior_division():
@@ -553,7 +526,7 @@ def senior_division():
   if senior_14 == "min":
     correct_point_hard()
   while senior_14 not in ["max", "min"]:
-    print("Please only enter you value in max an min")
+    print("Please only enter you value in max or in min")
     senior_14 = input(question_list_seniorplus[3]).strip().lower()
   if senior_14 == "max":
     wrong_point()
@@ -644,8 +617,14 @@ elif 10 <= age <= 14:
   timing = 0
   for timing in range(0,16):
     junior_input = input(random.choice(junior_quizbank))
+    while junior_input not in ["a","b","c","d"]:
+      print("Please only enter you value between a,b,c, and d")
+      junior_input = input(random.choice(junior_quizbank))
     junior_quizbank.remove(junior_input)
     timing = timing + 1
+  if timing == 15:
+    print("You have completed, you final score is {}".format(point))
+    exit()
 # Age between 15 and 18 years old will be allocate toward the senior division
 elif 15 <= age <= 18:
   print("You are in senior division!")
